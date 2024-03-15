7e748456649780701ffbf3aa132cf80d08574492
DrowAnn <rs19990801@gmail.com>
2024-03-15 08:17:12 -0500

    Scope

--- Clase1/clase1.js
+++ Clase1/clase1.js
@@ -1,1 +1,20 @@
-console.log("Hola Mundo");
+/* //Scope
+
+let variable = 44;
+
+function contador() {
+  let inicio = 0;
+
+  if (inicio == 0) {
+    let adicion = 1;
+    console.log(`${variable} ${inicio} ${adicion}`);
+  }
+  //console.log(`${variable} ${inicio} ${adicion}`); No accede a la variable "adicion" dentro del if
+  console.log(`${variable} ${inicio}`);
+}
+
+contador();
+//console.log(`${variable} ${inicio} ${adicion}`); No accede a las varibles "inicio" y "adicion" dentro de la funcion contador
+console.log(`${variable}`);*/
+
+//
