c77fa9d08e4e853dbb749fce09ac167efeac10b8
DrowAnn <rs19990801@gmail.com>
2024-03-15 11:45:41 -0500

    Clousures

--- Clase1/clase1.js
+++ Clase1/clase1.js
@@ -7,14 +7,43 @@ function contador() {
 
   if (inicio == 0) {
     let adicion = 1;
-    console.log(`${variable} ${inicio} ${adicion}`);
+    var sustraccion = 2
+    console.log(`${variable} ${inicio} ${adicion} ${sustraccion}`);
   }
-  //console.log(`${variable} ${inicio} ${adicion}`); No accede a la variable "adicion" dentro del if
-  console.log(`${variable} ${inicio}`);
+  //console.log(`${variable} ${inicio} ${adicion} ${sustraccion}`); No accede a la variable "adicion" dentro del if
+  console.log(`${variable} ${inicio} ${sustraccion}`); //Accede a "sustraccion" por que se declaro con var y no respeta scope de bloque
 }
 
 contador();
-//console.log(`${variable} ${inicio} ${adicion}`); No accede a las varibles "inicio" y "adicion" dentro de la funcion contador
+//console.log(`${variable} ${inicio} ${adicion} ${sustraccion}`); No accede a las varibles "inicio", "adicion" y "sustraccion" dentro de la funcion contador
 console.log(`${variable}`);*/
 
-//
+// Clousures
+
+function getset() {
+  let variable = 0;
+
+  function setVariable(nuevoValor) {
+    variable = nuevoValor;
+  }
+
+  function getVariable() {
+    return variable;
+  }
+
+  return {
+    get: getVariable,
+    set: setVariable,
+  };
+}
+
+//console.log(`${variable}`); No accede a "variable"
+let clase1 = getset();
+console.log("Valor inicial clase1: " + clase1.get());
+clase1.set(5);
+console.log("Valor despues del set a clase1: " + clase1.get());
+
+let clase2 = getset();
+console.log("Valor inicial clase2: " + clase2.get());
+clase2.set(8);
+console.log("Valor despues del set a clase2: " + clase2.get());
